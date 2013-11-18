eef97b4d6d9f0eedb517fdb6d56123e53b84fca8
Michael Pope <map7@corsairsolutions.com.au>
2013-11-18 21:17:50 +1100

    fix up yasnippet for ruby files

--- custom/04-yasnippet.el
+++ custom/04-yasnippet.el
@@ -3,5 +3,10 @@
 ;; (yas/initialize)
 ;; (yas/load-directory "~/.emacs.d/.cask/24.3.1/elpa/yasnippet-20131031.628/snippets") 
 
+;; Load ruby snippets
 (add-hook 'enh-ruby-mode-hook '(lambda ()
-			    (yas-minor-mode)))
+			    (yas-minor-mode)
+			    (yas-load-directory "~/.emacs.d/.cask/24.3.1/elpa/yasnippet-20131031.628/snippets/enh-ruby-mode")
+			    ))
+
+
