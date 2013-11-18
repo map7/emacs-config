1e4508d07d3de507060b4cb27f80945f3c5e90f9
Michael Pope <map7@corsairsolutions.com.au>
2013-11-19 08:59:55 +1100

    Add visual customisations

--- custom/00-common-setup.el
+++ custom/00-common-setup.el
@@ -18,7 +18,25 @@
 ;; Expand region shortcut
 (global-set-key (kbd "C-=") 'er/expand-region)
 
-;; Set color
+; General settings
+(setq-default tab-width 4)
+
+(custom-set-variables
+ '(column-number-mode t)
+ '(display-time-mode t)
+ '(ecb-options-version "2.32")
+ '(inhibit-startup-screen t)
+ '(max-lisp-eval-depth 3000)
+ '(max-specpdl-size 3000)
+ '(menu-bar-mode 1) 
+
+ '(rails-ws:default-server-type "mongrel")
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.googlemail.com")
+ '(smtpmail-smtp-service "smtp")
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,3 +50,24 @@
 
 ;; Remap hyper key as super
 (setq x-hyper-keysym 'super)
+
+
+;; (display-battery-mode)
+(setq column-number-mode t)
+(display-time)
+(setq backup-inhibited t) ;; disable backup
+
+;; Allow using mouse thumb button to browse URLs
+(global-set-key [mouse-10] 'browse-url-at-mouse)
+
+; stop emacs from contaminating each directory with semantic.cache
+(setq semanticdb-default-save-directory "/tmp")
+
+;; set scroll to step by 1 instead of half a page.
+(setq scroll-step            1
+      scroll-conservatively  10000)
+
+;fullscreen mode
+(defun switch-full-screen ()
+  (interactive)
+  (shell-command "wmctrl -r :ACTIVE: -btoggle,fullscreen"))
