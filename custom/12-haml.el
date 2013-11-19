7b7fe640e5eea384b72d777170c7a2fc8f8eb58a
Michael Pope <michael@dtcorp.com.au>
2013-11-19 13:52:29 +1100

    Add haml-ify

--- custom/12-haml.el
+++ custom/12-haml.el
@@ -2,3 +2,16 @@
 (require 'haml-mode nil 't)
  (add-hook 'haml-mode-hook
   '(lambda () (setq tab-width 2)))
+
+; html2haml function to convert current buffer to haml
+(defun haml-ify ()
+  "run html2haml on current buffer"
+  (interactive)
+  (setf filename buffer-file-name)
+  (setf newfilename (concat filename ".haml"))
+  (save-buffer)
+  (shell-command (concat
+	"html2haml " filename " > " newfilename))
+  (kill-buffer (current-buffer))
+  (delete-file filename)
+  (find-file newfilename))
