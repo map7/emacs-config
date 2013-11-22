295b40b4375c7ec2626bb16b64ebe92ef0a097e5
Michael Pope <michael@dtcorp.com.au>
2013-11-22 11:32:57 +1100

    Add my server scripts

---
+++ custom/33-emacs-server.el
@@ -0,0 +1,9 @@
+(global-set-key (kbd "C-!") 'server-shutdown) 
+
+;; define function to shutdown emacs server instance
+(defun server-shutdown ()
+  "Save buffers, Quit, and Shutdown (kill) server"
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs)
+  )
