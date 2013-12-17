00f9b5b6e94a05838c7120d357677599b03e10bc
Michael Pope <map7@corsairsolutions.com.au>
2013-12-17 21:16:55 +1100

    Add guide key & fix up scss-mode so that it doesn't compile on save as that is annoying

---
+++ custom/24-guide-key.el
@@ -0,0 +1,5 @@
+(require 'guide-key)
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 8"))
+(guide-key-mode 1) ; Enable guide-key-mode
+(setq guide-key/recursive-key-sequence-flag t)
+(setq guide-key/popup-window-position 'bottom)
---
+++ custom/25-scss-mode.el
@@ -0,0 +1,2 @@
+;; disable compile on save
+(setq scss-compile-at-save nil)
