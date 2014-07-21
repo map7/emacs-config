973dd764ed01635a9f81bc9001214c1dfd5a2203
Michael Pope <map7@corsairsolutions.com.au>
2014-07-21 20:52:14 +1000

    Upgrade all packages, 19-smart-mode-line not working, but Tramp mode is working better

--- custom/19-smart-mode-line.el
+++ custom/19-smart-mode-line.el
@@ -1,3 +1,3 @@
 ; configure the mode line at the bottom of the editor
 (require 'smart-mode-line)
-(sml/setup)
+(sml/setup)				
--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;; ;; An attempt to get smart-mode-line working
+;; ;; This allows the sml theme to be loaded without questions
+;; (custom-set-variables
+;;  '(custom-safe-themes (quote ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default))))
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
@@ -23,7 +28,9 @@
 (load "15-markdown.el")		 ; for md files.
 (load "17-rsense.el")		 ; Command Lookup
 (load "18-auto-complete.el") ; Auto complete
-(load "19-smart-mode-line.el")	; Configure the mode line at the bottom
+
+;; 21/07/2014 disabled due to issues with loading the theme
+;; (load "19-smart-mode-line.el")	; Configure the mode line at the bottom
 (load "21-coffeescript.el")	 ; Coffeescript mode
 (load "22-smex.el")			 ; smex (quick narrow down commands)
 (load "23-speedbar.el")		 ; Speedbar customisations
@@ -48,3 +55,54 @@
 (load "48-undo-tree.el")		; Undo tree
 (load "49-rbenv.el")			; Ruby version manager
 (load "50-org2blog.el")		 ; Org2blog mode
+
+;; Upgrade all packages
+(defun package-update-all ()
+  "Update all packages"
+  (interactive)
+  (dolist (elt package-alist)
+    (let* ((name (car elt))
+           (file-name (symbol-name name))
+           (available-pkg (assq name package-archive-contents))
+           (available-version (and available-pkg
+                                   (package-desc-vers (cdr available-pkg))))
+           (current-version (package-desc-vers (cdr elt)))
+           )
+      (when (and available-version
+                 (version-list-< current-version available-version))
+        (message "Updating to: %s - %s" file-name
+                 (package-version-join available-version))
+        (package-install name)
+        (package-delete file-name (package-version-join current-version))))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(display-time-mode t)
+ '(ecb-options-version "2.32")
+ '(inhibit-startup-screen t)
+ '(max-lisp-eval-depth 3000)
+ '(max-specpdl-size 3000)
+ '(menu-bar-mode 1)
+ '(rails-ws:default-server-type "mongrel")
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.googlemail.com")
+ '(smtpmail-smtp-service "smtp")
+ '(speedbar-show-unknown-files t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
