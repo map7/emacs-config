2e9aac4d4a090bf05895484397913b7ca79ef841
Michael Pope <map7@corsairsolutions.com.au>
2014-11-07 12:50:34 +1100

    Set my spell checker to british

--- custom/44-flyspell.el
+++ custom/44-flyspell.el
@@ -1,5 +1,6 @@
 (require 'flyspell)
 (setq flyspell-issue-message-flg nil)
+(setq ispell-dictionary "british")
 
 (add-hook 'enh-ruby-mode-hook
           (lambda () (flyspell-prog-mode)))
@@ -7,11 +8,16 @@
 (add-hook 'web-mode-hook
           (lambda () (flyspell-prog-mode)))
 
+(add-hook 'coffee-mode-hook
+	  (lambda () (flyspell-prog-mode)))
+
 (add-hook 'org-mode-hook
 	  (lambda () (flyspell-prog-mode)))
 
+
 (add-hook 'haml-mode-hook
 	  (lambda () ('flymake-haml-load)))
+
 
 ;; flyspell mode breaks auto-complete mode without this.
 (ac-flyspell-workaround)
