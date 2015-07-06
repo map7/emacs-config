8b2e5629a6522c16ed8988d14185e09c47a2e5ed
Michael Pope <map7@corsairsolutions.com.au>
2015-07-06 20:27:41 +1000

    fix auto save in org mode

--- custom/27-real-auto-save.el
+++ custom/27-real-auto-save.el
@@ -1,5 +1,5 @@
 ;; Automatically save the file
 ;; 
 (require 'real-auto-save)
-(add-hook 'org-mode-hook 'turn-on-real-auto-save)
+(add-hook 'org-mode-hook 'real-auto-save-mode)
 (setq real-auto-save-interval 5)        ; in seconds
