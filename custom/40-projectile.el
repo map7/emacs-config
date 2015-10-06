1e87fbf5dfd0cc5e9e8f4091c6f3773296a8fe72
Michael Pope <michael@dtcorp.com.au>
2015-10-06 11:35:11 +1100

    remove old work around for projectile

--- custom/40-projectile.el
+++ custom/40-projectile.el
@@ -12,15 +12,3 @@
 ;; Setup projectile-rails
 ;; This is stuffing around with tramp mode.
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
-
-;; 28/09/2015 - work around, ref;
-;;    https://github.com/bbatsov/projectile/issues/788
-;;    https://emacs.stackexchange.com/questions/14136/projectile-void-variable-it
-(defmacro --some (form list)
-  "Anaphoric form of `-some'."
-  (declare (debug (form form)))
-  (let ((n (make-symbol "needle")))
-    `(let (,n)
-       (--each-while ,list (not ,n)
-         (setq ,n ,form))
-       ,n)))
