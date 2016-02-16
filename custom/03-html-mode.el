40edd3868b5a988a4849ae90da5fecc74cbf4c72
Michael Pope <michael@dtcorp.com.au>
2016-02-16 16:22:05 +1100

    Make everything 2 spaces for tabs

--- custom/03-html-mode.el
+++ custom/03-html-mode.el
@@ -3,3 +3,8 @@
 
 (setq auto-mode-alist
       (cons '("\\.json" . js2-mode) auto-mode-alist))
+
+;; Set the correct indent of 2 spaces for everything
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
