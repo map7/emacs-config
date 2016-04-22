f426fb9d5f2272c043f6939b68bec5f565244572
Michael Pope <map7@corsairsolutions.com.au>
2016-04-22 11:53:39 +1000

    UTF8 support in multi-term

--- custom/11-multi-term.el
+++ custom/11-multi-term.el
@@ -2,3 +2,9 @@
 
 (global-set-key (kbd "C-x <up>") 'multi-term-next)
 (global-set-key (kbd "C-x <down>") 'multi-term-prev)
+
+;; Fix UTF8 characters in terminals
+(add-hook 'multi-term-exec-hook
+          (function
+           (lambda ()
+             (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
