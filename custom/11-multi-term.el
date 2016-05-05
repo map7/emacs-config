95e4d41fff059b92a2cbd14b32279b35c6ad07bd
Michael Pope <map7@corsairsolutions.com.au>
2016-05-05 21:22:25 +1000

    Fix emacs keys in mult-term

--- custom/11-multi-term.el
+++ custom/11-multi-term.el
@@ -8,3 +8,45 @@
           (function
            (lambda ()
              (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
+
+
+(defun last-term-buffer (l)
+  "Return most recently used term buffer."
+  (when l
+    (if (eq 'term-mode (with-current-buffer (car l) major-mode))
+        (car l) (last-term-buffer (cdr l)))))
+
+(defun get-term ()
+  "Switch to the term buffer last used, or create a new one if
+    none exists, or if the current buffer is already a term."
+  (interactive)
+  (let ((b (last-term-buffer (buffer-list))))
+    (if (or (not b) (eq 'term-mode major-mode))
+        (multi-term)
+      (switch-to-buffer b))))
+
+;; Fix moving around keys and disable yanking mode.
+(eval-after-load "term"
+  '(progn
+     ;; Fix forward/backward word when (term-in-char-mode).
+     (define-key term-raw-map (kbd "<C-left>")
+       (lambda () (interactive) (term-send-raw-string "\eb")))
+     (define-key term-raw-map (kbd "<M-left>")
+       (lambda () (interactive) (term-send-raw-string "\eb")))
+     (define-key term-raw-map (kbd "<C-right>")
+       (lambda () (interactive) (term-send-raw-string "\ef")))
+     (define-key term-raw-map (kbd "<M-right>")
+       (lambda () (interactive) (term-send-raw-string "\ef")))
+     ;; Disable killing and yanking in char mode (term-raw-map).
+     (mapc
+      (lambda (func)
+        (eval `(define-key term-raw-map [remap ,func]
+                 (lambda () (interactive) (ding)))))
+      '(backward-kill-paragraph
+        backward-kill-sentence backward-kill-sexp backward-kill-word
+        bookmark-kill-line kill-backward-chars kill-backward-up-list
+        kill-forward-chars kill-line kill-paragraph kill-rectangle
+        kill-region kill-sentence kill-sexp kill-visual-line
+        kill-whole-line kill-word subword-backward-kill subword-kill
+        yank yank-pop yank-rectangle))))
+(backward-kill-paragraph backward-kill-sentence backward-kill-sexp backward-kill-word bookmark-kill-line kill-backward-chars kill-backward-up-list kill-forward-chars kill-line kill-paragraph kill-rectangle kill-region ...)
