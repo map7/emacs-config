35e9d026f71b33e756a3817a867df064f3a46a82
Michael Pope <map7@corsairsolutions.com.au>
2016-05-07 15:44:27 +1000

    Remove faulty code for multi-term

--- custom/11-multi-term.el
+++ custom/11-multi-term.el
@@ -25,28 +25,29 @@
         (multi-term)
       (switch-to-buffer b))))
 
-;; Fix moving around keys and disable yanking mode.
-(eval-after-load "term"
-  '(progn
-     ;; Fix forward/backward word when (term-in-char-mode).
-     (define-key term-raw-map (kbd "<C-left>")
-       (lambda () (interactive) (term-send-raw-string "\eb")))
-     (define-key term-raw-map (kbd "<M-left>")
-       (lambda () (interactive) (term-send-raw-string "\eb")))
-     (define-key term-raw-map (kbd "<C-right>")
-       (lambda () (interactive) (term-send-raw-string "\ef")))
-     (define-key term-raw-map (kbd "<M-right>")
-       (lambda () (interactive) (term-send-raw-string "\ef")))
-     ;; Disable killing and yanking in char mode (term-raw-map).
-     (mapc
-      (lambda (func)
-        (eval `(define-key term-raw-map [remap ,func]
-                 (lambda () (interactive) (ding)))))
-      '(backward-kill-paragraph
-        backward-kill-sentence backward-kill-sexp backward-kill-word
-        bookmark-kill-line kill-backward-chars kill-backward-up-list
-        kill-forward-chars kill-line kill-paragraph kill-rectangle
-        kill-region kill-sentence kill-sexp kill-visual-line
-        kill-whole-line kill-word subword-backward-kill subword-kill
-        yank yank-pop yank-rectangle))))
-(backward-kill-paragraph backward-kill-sentence backward-kill-sexp backward-kill-word bookmark-kill-line kill-backward-chars kill-backward-up-list kill-forward-chars kill-line kill-paragraph kill-rectangle kill-region ...)
+;; ;; Fix moving around keys and disable yanking mode.
+;; (eval-after-load "term"
+;;   '(progn
+;;      ;; Fix forward/backward word when (term-in-char-mode).
+;;      (define-key term-raw-map (kbd "<C-left>")
+;;        (lambda () (interactive) (term-send-raw-string "\eb")))
+;;      (define-key term-raw-map (kbd "<M-left>")
+;;        (lambda () (interactive) (term-send-raw-string "\eb")))
+;;      (define-key term-raw-map (kbd "<C-right>")
+;;        (lambda () (interactive) (term-send-raw-string "\ef")))
+;;      (define-key term-raw-map (kbd "<M-right>")
+;;        (lambda () (interactive) (term-send-raw-string "\ef")))
+;;      ;; Disable killing and yanking in char mode (term-raw-map).
+;;      (mapc
+;;       (lambda (func)
+;;         (eval `(define-key term-raw-map [remap ,func]
+;;                  (lambda () (interactive) (ding)))))
+;;       '(backward-kill-paragraph
+;;         backward-kill-sentence backward-kill-sexp backward-kill-word
+;;         bookmark-kill-line kill-backward-chars kill-backward-up-list
+;;         kill-forward-chars kill-line kill-paragraph kill-rectangle
+;;         kill-region kill-sentence kill-sexp kill-visual-line
+;;         kill-whole-line kill-word subword-backward-kill subword-kill
+;;         yank yank-pop yank-rectangle))))
+;; (backward-kill-paragraph backward-kill-sentence backward-kill-sexp backward-kill-word bookmark-kill-line kill-backward-chars kill-backward-up-list kill-forward-chars kill-line kill-paragraph kill-rectangle kill-region ...)
+
