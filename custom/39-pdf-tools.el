0cd05573b0fd0e52d4713c4a459a6e58ec8fb9d7
Michael Pope <map7@corsairsolutions.com.au>
2016-05-09 20:51:39 +1000

    Add pdf bookmark facilities

--- custom/39-pdf-tools.el
+++ custom/39-pdf-tools.el
@@ -1,2 +1,16 @@
 ;; pdf-tools for viewing PDFs
 (pdf-tools-install)
+
+;; Set a bookmark on kill
+(defun kill-buffer-hook-setup ()
+  ;; Test that we have a filename and file extensions and it's a pdf and the user
+  ;; wants to take a bookmark
+  (if (and buffer-file-name
+           (file-name-extension buffer-file-name)
+           (string= (downcase (file-name-extension buffer-file-name)) "pdf")
+           (yes-or-no-p "Set bookmark with current file name?"))
+      ;; Set a bookmark with the name being the buffers full path name
+      (bookmark-set (file-name-nondirectory buffer-file-name) nil)))
+
+;; Setup a hook when we kill a pdf take a bookmark
+(add-hook 'kill-buffer-hook 'kill-buffer-hook-setup)
