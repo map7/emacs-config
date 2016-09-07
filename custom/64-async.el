6125bd9717201eb3d595677dad6c3ee667d92c69
Michael Pope <michael@dtcorp.com.au>
2016-09-08 09:16:24 +1000

    Add async abilities to dired
    This means that you can now copy/move large files around and emacs will
    do it in the background!

--- custom/64-async.el
+++ custom/64-async.el
@@ -1,2 +1,6 @@
 ;; Compile packages in the background.
 (async-bytecomp-package-mode 1)
+
+;; Allow copying in the background.
+(autoload 'dired-async-mode "dired-async.el" nil t)
+(dired-async-mode 1)
