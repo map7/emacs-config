3d0fa2ee4ca499d5247e73e4241e42b6efe8b192
Michael Pope <michael@dtcorp.com.au>
2016-09-26 15:06:38 +1000

    Fix search and replace across coffeescript files

--- custom/21-coffeescript.el
+++ custom/21-coffeescript.el
@@ -9,7 +9,8 @@
 (add-hook 'coffee-mode-hook
   '(lambda() (coffee-custom)))
 
-;; flymake-coffee
-(require 'flymake-coffee)
-(add-hook 'coffee-mode-hook 'flymake-coffee-load)
-(setq flymake-coffee-coffeelint-configuration-file (expand-file-name "~/.emacs.d/coffeelint.json"))
+;; ;; flymake-coffee
+;; ;; Removed 26/09/2016 as it stopped search and replace accross multiple coffeescript files from working.
+;; (require 'flymake-coffee)
+;; (add-hook 'coffee-mode-hook 'flymake-coffee-load)
+;; (setq flymake-coffee-coffeelint-configuration-file (expand-file-name "~/.emacs.d/coffeelint.json"))
