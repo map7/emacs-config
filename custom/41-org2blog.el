5b642b26cbfa2cc94c68797556f14213a2c27078
Michael Pope <michael@dtcorp.com.au>
2016-10-28 11:56:53 +1100

    Move personal org2blog stuff out to a home dir file.

--- custom/41-org2blog.el
+++ custom/41-org2blog.el
@@ -5,11 +5,16 @@
 (require 'netrc)
 (setq blog (netrc-machine (netrc-parse "~/.netrc") "2014roundtheworld" t))
 
-;; Login details for my round the world blog 2014
-(setq org2blog/wp-blog-alist
-      '(("2014roundtheworld"
-         :url "http://2014roundtheworld.wordpress.com/xmlrpc.php"
-         :username (netrc-get blog "login")
-         :password (netrc-get blog "password"))))
+(if (file-exists-p "~/.emacs.org2blog.el")
+    (load "~/.emacs.org2blog.el")
+  )
+
+;; Example
+;; ;; Login details for my round the world blog 2014
+;; (setq org2blog/wp-blog-alist
+;;       '(("2014roundtheworld"
+;;          :url "http://2014roundtheworld.wordpress.com/xmlrpc.php"
+;;          :username (netrc-get blog "login")
+;;          :password (netrc-get blog "password"))))
 
 
