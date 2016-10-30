74d83d337837126dcaa82f4df4041a3d6865d698
Andre Vidic <andre@dtcorp.com.au>
2016-10-30 23:23:05 +1100

    fix parenthesis for slock & hibernate shortcut

--- custom/98-exwm.el
+++ custom/98-exwm.el
@@ -20,10 +20,9 @@
 ;; + 'slock' is a simple X display locker provided by suckless tools. 'i3lock'
 ;;   is a more feature-rich alternative.
 (exwm-input-set-key (kbd "s-l")
-                    (lambda () (interactive) (start-process "" nil "slock")
-                      (start-process "" nil "sudo" "pm-suspend")
-                      )
-                    )
+                    (lambda () (interactive)
+                      (start-process "" nil "slock")
+                      (start-process "" nil "sudo" "pm-suspend")))
 
 ;; Redshift off
 (exwm-input-set-key (kbd "s-x")
