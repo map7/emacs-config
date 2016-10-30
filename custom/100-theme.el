711704f61e0e049635e2fde12761d9fa5ba3e893
Andre Vidic <andre@dtcorp.com.au>
2016-10-31 00:36:39 +1100

    Add theme options w/ comments

--- custom/100-theme.el
+++ custom/100-theme.el
@@ -1,3 +1,33 @@
-;; Load theme and set font
-(set-frame-font "Droid Sans Mono-10" nil t)
-(load-theme 'yoshi :no-confirm)
+;; Set font
+;; (set-frame-font "Droid Sans Mono-10" nil t)
+
+;; Load Theme
+;; Install base16-theme & jazz-theme through package manager
+
+(load-theme 'jazz t) ;; Best looking theme, just needs black bg to be perfect
+
+;; (load-theme 'base16-isotope t) ;; black bg, high contrast, great bright text
+;; (load-theme 'base16-pop t) ;; black bg, high contrast, nice
+;; (load-theme 'base16-pico t) ;; black bg, interesting, high contrast
+;; (load-theme 'base16-bright t) ;; black bg, high contrast
+;; (load-theme 'base16-ir-black t) ;; black bg, high contrast
+
+;; (load-theme 'base16-seti-ui t) ;; not black bg but dark, good colours
+;; (load-theme 'base16-solar-flare t) ;; not black bg but dark, good colours
+;; (load-theme 'base16-google-dark t) ;; not black bg but dark, good colours
+
+;; (load-theme 'base16-solarized-dark t);; not black bg but good colours
+;; (load-theme 'base16-spacemacs t) ;; not black bg but good colours
+
+;; (load-theme 'base16-phd t) ;; not black bg, colours not very high contrast
+;; (load-theme 'base16-tomorrow-night t) ;; not black bg
+;; (load-theme 'base16-twilight t) ;; not black bg
+
+;; (load-theme 'base16-summerfruit-dark t) ;; not great i think
+;; (load-theme 'base16-railscasts t) ;; not great i think
+;; (load-theme 'base16-macintosh t) ;; not great i think
+
+;;OTHER THEMES
+;;Danneskjold Theme
+;;https://github.com/emacs-jp/replace-colorthemes
+
