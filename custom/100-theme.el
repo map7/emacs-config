57d4903bfd722d75e6d214a401108c203a0314eb
Andre Vidic <andre@dtcorp.com.au>
2016-11-02 00:23:22 +1100

    Add more themes & settle on moe-theme

--- custom/100-theme.el
+++ custom/100-theme.el
@@ -1,33 +1,59 @@
 ;; Set font
 (set-frame-font "Droid Sans Mono-10" nil t)
 
-;; Load Theme
-;; Install base16-theme, danneskjold-theme & jazz-theme through package manager
+;; Load Theme(s)
+;; Through package manager first install following themes:
+;; * moe-theme
+;; * ample-theme
+;; * color-theme-sanityinc-tomorrow
+;; * jazz-theme
+;; * base16-theme
+;; * danneskjold-theme
+;; * badger-theme
 
-;; (load-theme 'jazz t) ;; Best looking theme, just needs black bg to be perfect
-(load-theme 'danneskjold t) ;; great simple black bg, high contrast theme
-;; (load-theme 'base16-isotope t) ;; black bg, high contrast, great bright text
-;; (load-theme 'base16-pop t) ;; black bg, high contrast, nice
-;; (load-theme 'base16-pico t) ;; black bg, interesting, high contrast
-;; (load-theme 'base16-bright t) ;; black bg, high contrast
-;; (load-theme 'base16-ir-black t) ;; black bg, high contrast
+;; BEST THEME: Moe-theme best documentation, easily customisable & documented
+;; Add proper powerline integration
 
-;; (load-theme 'base16-seti-ui t) ;; not black bg but dark, good colours
-;; (load-theme 'base16-solar-flare t) ;; not black bg but dark, good colours
-;; (load-theme 'base16-google-dark t) ;; not black bg but dark, good colours
+  (require 'moe-theme) 
+;; Choose a color for mode-line.(Default: blue)
+  (moe-theme-set-color 'w/b)
+;; Resize titles
+  (setq moe-theme-resize-markdown-title '(2.0 1.7 1.5 1.3 1.0 1.0))
+  (setq moe-theme-resize-org-title '(2.2 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
+  (setq moe-theme-resize-rst-title '(2.0 1.7 1.5 1.3 1.1 1.0))
 
-;; (load-theme 'base16-solarized-dark t);; not black bg but good colours
-;; (load-theme 'base16-spacemacs t) ;; not black bg but good colours
+;; Initialise powerline-moe-theme
+  (powerline-moe-theme)
+;; Load theme with customisations applied
+  (moe-dark) 
 
+;; OKAY THEMES
+;; (load-theme 'ample t) ;;black bg, good normal colours, ok
+;; (load-theme 'sanityinc-tomorrow-bright t) ;;black bg, good normal colours
+;; (load-theme 'jazz t) ;; Good looking theme, just needs black bg to be perfect
+;; (load-theme 'danneskjold t) ;; great simple black bg, high contrast theme
+;; (load-theme 'base16-pico t) ;; black bg, high contrast, pink highlighting...
+;; (load-theme 'base16-bright t) ;; black bg, modeline not as good as others...
+;; (load-theme 'base16-solarized-dark t);; not black bg, good colours, grey fog
+
+;; TOLERABLE
+;; (load-theme 'base16-seti-ui t) ;; dark bg, horrible modeline & linum green...
+;; (load-theme 'base16-solar-flare t) ;; dark bg, grey looks like fog...
+;; (load-theme 'base16-google-dark t) ;; not black bg but dark, colours ok
+;; (load-theme 'base16-spacemacs t) ;; dark bg, ok colours, purple do & end...
 ;; (load-theme 'base16-phd t) ;; not black bg, colours not very high contrast
 ;; (load-theme 'base16-tomorrow-night t) ;; not black bg
 ;; (load-theme 'base16-twilight t) ;; not black bg
+;; (load-theme 'base16-ir-black t) ;; black bg, grape do & end blocks...
+;; (load-theme 'base16-pop t) ;; black bg, uses purple do & end blocks...
+;; (load-theme 'base16-isotope t) ;; black bg, high contrast, pink & purple keywords...
 
+;; CRAP
+;; (load-theme 'badger t) ;; Cant read modeline, Black highlighting on dark bg!!!
 ;; (load-theme 'base16-summerfruit-dark t) ;; not great i think
 ;; (load-theme 'base16-railscasts t) ;; not great i think
 ;; (load-theme 'base16-macintosh t) ;; not great i think
 
 ;;OTHER THEMES
-;;Danneskjold Theme
 ;;https://github.com/emacs-jp/replace-colorthemes
 
