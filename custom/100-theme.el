209317ce029745eb79760df85c1189f9bbbb97e6
Andre Vidic <andre@dtcorp.com.au>
2016-11-02 15:39:24 +1100

    100-theme: Customise Magit diff add/remove branch local...

--- custom/100-theme.el
+++ custom/100-theme.el
@@ -33,13 +33,30 @@
 ;; Set background colour to black
 (set-face-background 'default "#000000")
 
-;; Load theme with customisations applied
-(moe-dark) 
-
-;; Set the default mouse face.
 (custom-set-faces
- '(mouse ((t (:background "grey")))))
+;; Set the default mouse face.
+ '(mouse ((t (:background "grey"))))
 
+;;---Magit---
+ ;; Set Magit Diff Added
+ '(magit-diff-added ((t (:foreground "#2BFF00"))))
+
+  ;; Set Magit Diff Removed
+ '(magit-diff-removed ((t (:foreground "#FF0000"))))
+
+ ;; Set Magit Branch Local
+ '(magit-branch-local (((class color)
+                        (background light))
+                       (:foreground "#SkyBlue4"))
+                      ((class color)
+                       (background dark))
+                      (:foreground "#1FBED6"))
+
+ ;; Set Magit Current Branch Face
+ '(magit-branch-current ((t (:box
+                             (:line-width 1 :color nil :style nil)
+                             :inherit (magit-branch-local)))))
+)
 
 ;; OKAY THEMES
 ;; (load-theme 'ample t) ;;black bg, good normal colours, ok
