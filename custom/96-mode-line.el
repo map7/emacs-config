050e43cfd2c257f7785bbd4e7ba811de4ac42fdd
Andre Vidic <andre@dtcorp.com.au>
2016-11-04 00:10:18 +1100

    96-mode-line: Use lunaryorn git vc (not working...)

--- custom/96-mode-line.el
+++ custom/96-mode-line.el
@@ -14,10 +14,12 @@
 
 ;; Remove the 'git' component
 ;; http://www.lunaryorn.com/2014/07/26/make-your-emacs-mode-line-more-useful.html
+(defvar lunaryorn-vc-mode-line
   '(" " (:propertize
          ;; Strip the backend name from the VC status information
          (:eval (let ((backend (symbol-name (vc-backend (buffer-file-name)))))
                   (substring vc-mode (+ (length backend) 2))))
+         face font-lock-variable-name-face))
   "Mode line format for VC Mode.")
 (put 'lunaryorn-vc-mode-line 'risky-local-variable t)
 
