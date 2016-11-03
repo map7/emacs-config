20c3e46579c095396507b43a3e8bdf28c05aedcd
Andre Vidic <andre@dtcorp.com.au>
2016-11-04 00:08:57 +1100

    96-mode-line: Set mode-line up using definitions

--- custom/96-mode-line.el
+++ custom/96-mode-line.el
@@ -12,3 +12,27 @@
                   (substring vc-mode (+ (length backend) 2))))
   "Mode line format for VC Mode.")
 (put 'lunaryorn-vc-mode-line 'risky-local-variable t)
+
+;; Setup my mode line
+(setq-default mode-line-format
+  (list "%e" ; print error message about full memory.
+        '(:eval (append
+                 (list
+                  (powerline-lcl             'left nil  )
+                  (powerline-rmw             'left nil  )
+                  (powerline-buffer-id       'left nil   powerline-color1                 )
+                  (powerline-major-mode      'left       powerline-color1                 )
+                  (powerline-minor-modes     'left       powerline-color1                 )
+                  (powerline-narrow          'left       powerline-color1 powerline-color2)
+                  (lunaryorn-vc-mode-line    'center     powerline-color2                 )
+
+            (powerline-pull-right (list 
+                  (powerline-row             'right     powerline-color1 powerline-color2)
+                  (powerline-make-text       ":"        powerline-color1)
+                  (powerline-column          'right     powerline-color1)
+                  (powerline-percent         'right nil powerline-color1)
+                  (powerline-display-time    'right nil )
+                  (powerline-display-battery 'right nil)
+                  (powerline-make-text       " "    nil)))
+            )))))
+
