e13b36d1040290bfa75b1a0e796db635c223a944
Michael Pope <michael@dtcorp.com.au>
2016-12-16 12:10:53 +1100

    Spacing

--- custom/00-common-setup.el
+++ custom/00-common-setup.el
@@ -27,14 +27,14 @@
 
 ;; Setup external directory variable
 (setq elisp-external-dir
-	  (expand-file-name "external" user-emacs-directory))
+    (expand-file-name "external" user-emacs-directory))
 
 ;; Add external projects to load path
 (add-to-list 'load-path elisp-external-dir)
 
 (dolist (project (directory-files elisp-external-dir t "\\w+"))
   (when (file-directory-p project)
-	(add-to-list 'load-path project)))
+  (add-to-list 'load-path project)))
 
 ; Auto revert unless there is unsaved data
 (global-auto-revert-mode t)
