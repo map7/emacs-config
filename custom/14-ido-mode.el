1b6d91f18c370bd43976089f4752b700afd32cf3
Michael Pope <michael@dtcorp.com.au>
2017-01-13 16:49:14 +1100

    Fix up two use-packages

--- custom/14-ido-mode.el
+++ custom/14-ido-mode.el
@@ -1,6 +1,2 @@
 ;; Load the Interactive Do Things tool, which makes scaling dirs quicker
-(use-package
-  :init
-  (ido-mode t)
-  :defer 5)
-
+(ido-mode t)
--- custom/85-yaml_mode.el
+++ custom/85-yaml_mode.el
@@ -1,4 +1,4 @@
-(use-package yaml-mode,
+(use-package yaml-mode
   :init
   (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
   :defer)
