6a635c8749ddb004360a4fda187957239472de30
Michael Pope <michael@dtcorp.com.au>
2017-01-13 14:23:17 +1100

    Add org-lint check on saving org files.
    This was done to try to minimise the errors in my org files and help
    publishing them.

--- custom/40-org.el
+++ custom/40-org.el
@@ -69,5 +69,27 @@
         ;; ...other commands here
         ))
 
+(require 'flycheck)
 ;; Display images inline automatically
 (setq org-startup-with-inline-images t)
+
+(defun flycheck-org-lint-start (checker callback)
+  (funcall
+   callback 'finished
+   (save-excursion
+     (mapcar
+      (lambda (err)
+        (goto-char (car err))
+        (flycheck-error-new-at
+         (org-current-line) (1+ (current-column))
+         'warning (cadr err) :checker checker))
+      (org-lint-link-to-local-file (org-element-parse-buffer))
+      ))))
+
+;; Using org-lint check the health of our org file on save
+(flycheck-define-generic-checker 'org-lint
+  "Syntax checker for org-lint."
+  :start 'flycheck-org-lint-start
+  :modes '(org-mode))
+
+(add-to-list 'flycheck-checkers 'org-lint)
