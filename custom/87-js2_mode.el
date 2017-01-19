0406559add991eecc521cfd721031eb34e85432c
Michael Pope <map7@corsairsolutions.com.au>
2017-01-19 23:27:13 +1100

    Change tabs

--- custom/87-js2_mode.el
+++ custom/87-js2_mode.el
@@ -1,6 +1,7 @@
 ;; Javascript files.
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(setq js-indent-level 2)                ; Set tabs for js files
 
 ;; JSX mode
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
