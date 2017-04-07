aec6232dd9cf21388897698317be41a7b014229e
Michael Pope <michael@dtcorp.com.au>
2017-04-07 16:25:06 +1000

    Add magithub

--- custom/53-magit.el
+++ custom/53-magit.el
@@ -10,3 +10,7 @@
 
   (eval-after-load "magit"
     '(define-key magit-status-mode-map (kbd "C-c C-a") 'magit-just-amend)))
+
+(use-package magithub
+  :after magit
+  :config (magithub-feature-autoinject t))
