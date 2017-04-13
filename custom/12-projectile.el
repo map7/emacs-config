f0a914956bc81aef4d702c475a26203810955849
Michael Pope <michael@dtcorp.com.au>
2017-04-13 14:13:34 +1000

    Disable old projectile

--- custom/12-projectile.el
+++ custom/12-projectile.el
@@ -13,7 +13,7 @@
   (setq projectile-completion-system 'grizzl)
 
   ;; Press Super-p for fuzzy find in project
-  (global-set-key (kbd "s-f") 'projectile-find-file)
+  ;; (global-set-key (kbd "s-f") 'projectile-find-file)
 
   ;; Press Super-b for fuzzy switch buffer
   (global-set-key (kbd "s-b") 'projectile-switch-to-buffer))
