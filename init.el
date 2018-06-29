c09ede297ae2101d67476222ceb6f3b793797981
Andre Vidic <andre@dtcorp.com.au>
2018-06-29 11:45:57 +1000

    Init-auto install alect-themes

--- init.el
+++ init.el
@@ -54,14 +54,17 @@
 (use-package xkcd :defer 5)
 (use-package dockerfile-mode :defer 5)
 (use-package nov)
+;; load theme called alect-themes
+(use-package alect-themes
+  :ensure t
+  :init
+  (load-theme 'alect-black t))
 
 ;; All custom files have a number at the front so they don't clash with the library files.
 
 ;; 4gl mode from external
 (require '4gl-mode)
 
-;; load theme
-(load-theme 'alect-black t)
 
 ;;--------------------------------------------------------------------------------
 ;;-Base emacs related (0-39)
