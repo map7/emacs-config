a68bee3da83a7f1345d79a41bf24aee6255f3a2b
Andre Vidic <andre@dtcorp.com.au>
2018-09-07 11:55:04 +1000

    Modeline-Add ERC to modeline

--- custom/101-modeline.el
+++ custom/101-modeline.el
@@ -98,18 +98,8 @@
          '((t (:foreground "black" :background "indianred")))
          "Face to use when ERC has been disconnected.")
        
-       (defun erc-update-header-line-show-disconnected ()
-         "Use a different face in the header-line when disconnected."
-         (erc-with-server-buffer
-           (cond ((erc-server-process-alive) 'erc-header-line)
-                 (t 'erc-header-line-disconnected))))
-       (setq erc-header-line-face-method 'erc-update-header-line-show-disconnected)
-
-       " "
-       '(:eval 'Target-and/or-server)
-
-	     " "
-       '(:eval 'status 'away)
+       ;; ERC
+	     'mode-line-misc-info
        
        ;; == Window ==
        ;; '(:eval (window-parameter (selected-window) 'ace-window-path))
