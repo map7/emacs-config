713135fde18ef85b368466eccb124aea514e4c2a
Michael Pope <michael@dtcorp.com.au>
2018-09-11 08:37:02 +1000

    Init-Remark out package-init

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 (add-to-list 'load-path "~/.emacs.d/external") ; Plugins not in the repo
 (add-to-list 'load-path "~/.emacs.d/external/4gl-mode-master")
 
-(package-initialize)
+;;(package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
