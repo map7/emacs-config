765e227423ff7cd6b86c6c837770f9ec6115a2b5
Michael Pope <michael@dtcorp.com.au>
2019-07-03 13:35:10 +1000

    Flycheck-REFACTOR-Move flycheck code to the flycheck file

--- custom/40-org.el
+++ custom/40-org.el
@@ -71,34 +71,8 @@
         ;; ...other commands here
         ))
 
-(require 'flycheck)
 ;; Display images inline automatically
 (setq org-startup-with-inline-images t)
-
-;;;; Takes too long to do the check.
-;; (defun flycheck-org-lint-start (checker callback)
-;;   (funcall
-;;    callback 'finished
-;;    (save-excursion
-;;      (mapcar
-;;       (lambda (err)
-;;         (goto-char (car err))
-;;         (flycheck-error-new-at
-;;          (org-current-line) (1+ (current-column))
-;;          'warning (cadr err) :checker checker))
-;;       (org-lint '(link-to-local-file))
-;;       ))))
-
-;; ;; Using org-lint check the health of our org file on save
-;; (flycheck-define-generic-checker 'org-lint
-;;   "Syntax checker for org-lint."
-;;   :start 'flycheck-org-lint-start
-;;   :modes '(org-mode))
-
-;; (add-to-list 'flycheck-checkers 'org-lint)
-
-
-
 
 (defun org-clock-sum-agenda-today ()
   "Visit each file in `org-agenda-files' and return the total time of today's clocked tasks in minutes."
--- custom/66-flycheck.el
+++ custom/66-flycheck.el
@@ -1,5 +1,27 @@
 (use-package flycheck
   :init
   (global-flycheck-mode)
+  :ensure t
   :defer)
 
+;;;; Takes too long to do the check.
+;; (defun flycheck-org-lint-start (checker callback)
+;;   (funcall
+;;    callback 'finished
+;;    (save-excursion
+;;      (mapcar
+;;       (lambda (err)
+;;         (goto-char (car err))
+;;         (flycheck-error-new-at
+;;          (org-current-line) (1+ (current-column))
+;;          'warning (cadr err) :checker checker))
+;;       (org-lint '(link-to-local-file))
+;;       ))))
+
+;; ;; Using org-lint check the health of our org file on save
+;; (flycheck-define-generic-checker 'org-lint
+;;   "Syntax checker for org-lint."
+;;   :start 'flycheck-org-lint-start
+;;   :modes '(org-mode))
+
+;; (add-to-list 'flycheck-checkers 'org-lint)
