f11e63c264d0b39e7f5a33c21c05eb9ea6aa5683
Andre Vidic <andre@dtcorp.com.au>
2019-10-10 16:51:09 +1100

    Theme-Add a light theme

--- themes/customizable-light-theme.el
+++ themes/customizable-light-theme.el
@@ -207,8 +207,8 @@
 
  ;; Magit
  '(magit-diff-context-highlight ((t :background "white")))
- '(magit-diff-file-heading ((t :weight bold :foreground "cyan3")))
- '(magit-diff-file-heading-highlight ((t :weight bold :foreground "cyan3" :background "NavyBlue")))
+ '(magit-diff-file-heading ((t :weight bold :foreground "cyan")))
+ '(magit-diff-file-heading-highlight ((t :weight bold :foreground "cyan" :background "NavyBlue")))
  '(magit-diff-removed-highlight ((t :inherit diff-removed)))
  '(magit-diff-removed ((t :inherit diff-removed)))
  '(magit-diff-added-highlight ((t :inherit diff-added)))
