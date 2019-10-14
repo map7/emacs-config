b915010104afe17e2f81936fde9a9a885f8b60b0
Andre Vidic <andre@dtcorp.com.au>
2019-10-14 17:47:26 +1100

    Theme-light-replace cyan w darker blue in diff file heading

--- themes/customizable-light-theme.el
+++ themes/customizable-light-theme.el
@@ -206,8 +206,8 @@
 
  ;; Magit
  '(magit-diff-context-highlight ((t :background "white")))
- '(magit-diff-file-heading ((t :weight bold :foreground "cyan")))
- '(magit-diff-file-heading-highlight ((t :weight bold :foreground "cyan" :background "NavyBlue")))
+ '(magit-diff-file-heading ((t :weight bold :foreground "DodgerBlue1")))
+ '(magit-diff-file-heading-highlight ((t :foreground "DodgerBlue1" :background "cyan" :weight bold)))
  '(magit-diff-removed-highlight ((t :inherit diff-removed)))
  '(magit-diff-removed ((t :inherit diff-removed)))
  '(magit-diff-added-highlight ((t :inherit diff-added)))
