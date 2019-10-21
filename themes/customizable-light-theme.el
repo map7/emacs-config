92835b40f8496280451dc90e6c44f453ca2d74c2
Andre Vidic <andre@dtcorp.com.au>
2019-10-22 07:57:53 +1100

    Theme-light-update last default face colours & correctly order

--- themes/customizable-light-theme.el
+++ themes/customizable-light-theme.el
@@ -61,6 +61,11 @@
  '(mode-line-emphasis ((t (:weight bold))))
  '(mode-line-highlight ((t (:box nil :weight bold))))
  '(mode-line-inactive ((t (:inherit mode-line :background "white" :foreground "black" :box (:line-width 1 :color "DimGray") :weight light))))
+ ;; iSearch
+ '(isearch ((t (:background "DodgerBlue4" :foreground "light sky blue"))))
+ '(isearch-fail ((t (:background "orange red" :foreground "black"))))
+ '(lazy-highlight ((t (:background "dim gray" :foreground "gold3"))))
+ '(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground "white" :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
  '(next-error ((t (:inherit (region)))))
  '(query-replace ((t (:inherit (isearch)))))
 
