0968957bc30133ed1c388250c5a26fff28a8d7c8
Andre Vidic <andre@dtcorp.com.au>
2019-10-23 10:24:57 +1100

    Theme-light-Switch between light & dark theme using s-t (was term)
    Reassigned s-t from open term to toggle theme shortcut

--- custom/100-theme.el
+++ custom/100-theme.el
@@ -4,5 +4,25 @@
 ;; Trust custom themes
 (setq custom-safe-themes t)
 
-;; Load custom theme
-(load-theme 'customizable-dark)
+;; Create theme toggle function
+(setq my/themes '(customizable-light customizable-dark))
+(setq my/themes-index 0)
+
+(defun my/cycle-theme ()
+  (interactive)
+  (setq my/themes-index (% (1+ my/themes-index) (length my/themes)))
+  (my/load-indexed-theme))
+
+(defun my/load-indexed-theme ()
+  (my/try-load-theme (nth my/themes-index my/themes)))
+
+(defun my/try-load-theme (theme)
+  (if (ignore-errors (load-theme theme :no-confirm))
+      (mapcar #'disable-theme (remove theme custom-enabled-themes))
+    (message "Unable to find theme file for ‘%s’" theme)))
+
+;; Set theme toggle shortcut key
+(global-set-key (kbd "s-t") 'my/cycle-theme)
+
+;; Load theme toggle function & load theme
+(my/load-indexed-theme)
