525bba3c8d3330668e37765218c868b5f412b8bb
Michael Pope <map7777@gmail.com>
2019-11-11 22:16:28 +1100

    Install i3

--- custom/94-i3.el
+++ custom/94-i3.el
@@ -1,6 +1,8 @@
 ;; Start a server
 (server-start)
 
-;; to start other emacs windows in i3 use 's-d emacsclient -c'
+;; to start other emacs windows in i3 use 's-d emacsclient -c' which you can use super+c
+(use-package i3wm
+  :ensure t)
 
 
