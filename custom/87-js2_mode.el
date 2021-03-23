c0baa9b992fd1c5a958ea89de8d8d577079cf4e3
Michael Pope <michael@dtcorp.com.au>
2021-03-24 09:27:42 +1100

    JS-Remove warning about JSX support

--- custom/87-js2_mode.el
+++ custom/87-js2_mode.el
@@ -3,6 +3,11 @@
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 (setq js-indent-level 2)                ; Set tabs for js files
 
-;; JSX mode
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+
+;; --------------------------------------------------------------------------------
+;; The following creates error;
+;; Warning (js2-mode): For JSX support, use js-mode with js2-minor-mode
+;; --------------------------------------------------------------------------------
+;; ;; JSX mode
+;; (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+;; (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
