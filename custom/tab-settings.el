88d90cd267c6919f3658831b773300462dae8f16
Michael Pope <michael@dtcorp.com.au>
2023-09-20 16:37:08 +1000

    Fix weird tab issue with 4gl files when hitting C-j

--- custom/tab-settings.el
+++ custom/tab-settings.el
@@ -1,4 +1,5 @@
 ;; Tab settings
-(define-key global-map (kbd "C-j") 'newline-and-indent);; auto-indent
+(define-key global-map (kbd "C-j") 'electric-newline-and-maybe-indent)
+(define-key global-map (kbd "C-m") 'electric-newline-and-maybe-indent)
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
