56658516729a0f71ce64eb44efc8e23f88f42c55
Michael Pope <michael@dtcorp.com.au>
2023-11-29 12:11:11 +1100

    Setup newline

--- custom/tab-settings.el
+++ custom/tab-settings.el
@@ -1,5 +1,12 @@
 ;; Tab settings
-(define-key global-map (kbd "C-j") 'electric-newline-and-maybe-indent)
-(define-key global-map (kbd "C-m") 'electric-newline-and-maybe-indent)
+
+;; ;; 2023-TODO: use these for 4gl mode
+;; (define-key global-map (kbd "C-j") 'electric-newline-and-maybe-indent)
+;; (define-key global-map (kbd "C-m") 'electric-newline-and-maybe-indent)
+
+;; 2023-TODO: use these for everything else
+(define-key global-map (kbd "C-j") 'newline-and-indent)
+(define-key global-map (kbd "C-m") 'newline-and-indent)
+
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
