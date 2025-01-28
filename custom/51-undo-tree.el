d080123a716465e707d08dfd9d41190629d5a521
Michael Pope <michael@dtcorp.com.au>
2025-01-28 11:18:47 +1100

    Move undotree temp files to one dir

--- custom/51-undo-tree.el
+++ custom/51-undo-tree.el
@@ -1,5 +1,8 @@
 (use-package undo-tree
   :init
   (global-undo-tree-mode)
+  :config
+  ;; Prevent undo tree files from polluting your git repo
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
   :defer 5)
 
