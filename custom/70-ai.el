a8702e0dbb5236376c1c8a892eeabb0034898324
Michael Pope <michael@dtcorp.com.au>
2025-05-16 11:36:14 +1000

    Adding gptel

--- custom/70-ai.el
+++ custom/70-ai.el
@@ -1,6 +1,23 @@
+;; c3po was the first interaction I got with ChatGPT in Emacs
 (use-package c3po
   :straight (:host github :repo "d1egoaz/c3po.el"))
 
 (if (file-exists-p "~/.emacs.c3po.el")
     (load "~/.emacs.c3po.el")  ; Email settings
 )
+
+;; gptel is a LLM chat client for Emacs which handles heaps of models & backends.
+(straight-use-package 'gptel)
+
+(if (file-exists-p "~/.emacs.gptel.el")
+    (load "~/.emacs.gptel.el") 
+)
+
+;; Llamafile
+(setq
+ gptel-model   'test
+ gptel-backend (gptel-make-openai "llama-cpp"
+                 :stream t
+                 :protocol "http"
+                 :host "localhost:8080"
+                 :models '(test)))
