a806facc444aa4badab8d4b7bc5017171c534c9f
Michael Pope <map7777@gmail.com>
2025-06-11 09:18:02 +1000

    Config gptel

--- custom/70-ai.el
+++ custom/70-ai.el
@@ -1,3 +1,4 @@
+;; --------------------------------------------------------------------------------
 ;; c3po was the first interaction I got with ChatGPT in Emacs
 (use-package c3po
   :straight (:host github :repo "d1egoaz/c3po.el"))
@@ -13,6 +14,27 @@
 ;;     (load "~/.emacs.gptel.el") 
 ;; )
 
+;; ;; --------------------------------------------------------------------------------
+;; ;; gptel
+;; ;; Is a LLM chat client for Emacs which handles heaps of models & backends.
+
+;; (use-package gptel
+;;   :ensure t
+;;   :config
+;;   (setq gptel-api-key
+;;         (let ((auth-info (car (auth-source-search
+;;                                :host "api.openai.com"
+;;                                :user "openai"
+;;                                :require '(:user :secret)))))
+;;           (message "Auth info: %S" auth-info)  ;; Debug print
+;;           (when auth-info
+;;             (let ((secret (plist-get auth-info :secret)))
+;;               (if (functionp secret)
+;;                   (funcall secret)
+;;                 secret)))))
+;;   :bind ("C-c g" . 'gptel)
+;;   )
+
 ;; ;; Llamafile
 ;; (setq
 ;;  gptel-model   'test
@@ -21,3 +43,21 @@
 ;;                  :protocol "http"
 ;;                  :host "localhost:8080"
 ;;                  :models '(test)))
+<<<<<<< HEAD
+=======
+
+
+;; --------------------------------------------------------------------------------
+;; Ellama
+(use-package ellama
+  :ensure t
+  :bind ("C-c e" . ellama)
+  ;; send last message in chat buffer with C-c C-c
+  :hook (org-ctrl-c-ctrl-c-final . ellama-chat-send-last-message)
+  :init (setopt ellama-auto-scroll t)
+  :config
+  ;; show ellama context in header line in all buffers
+  (ellama-context-header-line-global-mode +1)
+  ;; show ellama session id in header line in all buffers
+  (ellama-session-header-line-global-mode +1))
+>>>>>>> 827478e (Config gptel)
