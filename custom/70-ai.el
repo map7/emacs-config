3ec44befc9b9bdd6726fd7e8e614264e8a236718
Michael Pope <michael@dtcorp.com.au>
2025-07-14 10:55:08 +1000

    AI-Enable gptel

--- custom/70-ai.el
+++ custom/70-ai.el
@@ -3,16 +3,15 @@
 (use-package c3po
   :straight (:host github :repo "d1egoaz/c3po.el"))
 
-(if (file-exists-p "~/.emacs.c3po.el")
-    (load "~/.emacs.c3po.el")  ; Email settings
-)
+(when (file-exists-p "~/.emacs.c3po.el")
+    (load "~/.emacs.c3po.el"))  ; Email settings
 
-;; ;; gptel is a LLM chat client for Emacs which handles heaps of models & backends.
-;; (straight-use-package 'gptel)
 
-;; (if (file-exists-p "~/.emacs.gptel.el")
-;;     (load "~/.emacs.gptel.el") 
-;; )
+;; gptel is a LLM chat client for Emacs which handles heaps of models & backends.
+(straight-use-package 'gptel)
+
+(when (file-exists-p "~/.emacs.gptel.el")
+  (load "~/.emacs.gptel.el"))
 
 ;; ;; --------------------------------------------------------------------------------
 ;; ;; gptel
