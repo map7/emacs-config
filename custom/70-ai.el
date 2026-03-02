14abf38387aaee34ca698c5c188bb06812b702ef
Michael Pope <michael@dtcorp.com.au>
2026-03-02 15:32:19 +1100

    AI-Add claude code integration

--- custom/70-ai.el
+++ custom/70-ai.el
@@ -1,3 +1,47 @@
+;; March, 2026 - Claude Code Integration
+
+;; install required inheritenv dependency:
+(use-package inheritenv
+  :vc (:url "https://github.com/purcell/inheritenv" :rev :newest))
+
+;; for eat terminal backend:
+(use-package eat :ensure t)
+
+;; for vterm terminal backend:
+(use-package vterm :ensure t)
+
+;; install claude-code.el
+(use-package monet
+  :vc (:url "https://github.com/stevemolitor/monet"
+       :rev :newest))
+
+(use-package claude-code
+  :vc (:url "https://github.com/stevemolitor/claude-code"
+       :rev :newest)
+  :after monet
+  :config
+  (add-hook 'claude-code-process-environment-functions
+            #'monet-start-server-function)
+  (monet-mode 1)
+  (claude-code-mode)
+e  :bind-keymap ("C-c c" . claude-code-command-map)
+  )
+
+
+;; (use-package claude-code :ensure t
+;;   :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+;;   :config
+;;   ;; optional IDE integration with Monet
+;;   (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
+;;   (monet-mode 1)
+
+;;   (claude-code-mode)
+;;   :bind-keymap ("C-c c" . claude-code-command-map)
+
+;;   ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+;;   :bind
+;;   (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode)))
+
 ;; --------------------------------------------------------------------------------
 ;; c3po was the first interaction I got with ChatGPT in Emacs
 (use-package c3po
