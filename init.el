d3ca4338e4e01e4908f355991d8a019edd01a7e7
Michael Pope <michael@dtcorp.com.au>
2026-03-02 15:32:36 +1100

    Remove custom use-package, Emacs has this inbuilt now

--- init.el
+++ init.el
@@ -28,12 +28,13 @@
 
 
 
-;; Setup use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
+;; ;; Setup use-package
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+;; (eval-when-compile
+;;   (require 'use-package))
+
 (setq use-package-always-ensure t)
 
 ;; Setup straight
