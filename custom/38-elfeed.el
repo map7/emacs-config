d56146c8de387b02d4bf4fb38c0f5e9c2272e112
Michael Pope <michael@dtcorp.com.au>
2026-04-15 13:20:14 +1000

    elfeed-spacing

--- custom/38-elfeed.el
+++ custom/38-elfeed.el
@@ -10,32 +10,32 @@
           ("https://rss.buzzsprout.com/1761225.rss" podcast accounting)
 
           ;; Podcasts - Programming / Ruby
-          ("https://rss.buzzsprout.com/1878319.rss" podcast ruby)           ; Code with Jason
-          ("https://www.spreaker.com/show/6102073/episodes/feed" podcast ruby) ; Ruby Rogues
-          ("https://rss.buzzsprout.com/2260490.rss" podcast ruby)           ; Remote Ruby
-          ("https://bikeshed.thoughtbot.com/rss" podcast ruby)              ; The Bike Shed
-          ("https://www.therubyonrailspodcast.com/rss" podcast ruby rails)  ; Ruby on Rails Podcast
+          ("https://rss.buzzsprout.com/1878319.rss" podcast ruby)                                        ; Code with Jason
+          ("https://www.spreaker.com/show/6102073/episodes/feed" podcast ruby)                           ; Ruby Rogues
+          ("https://rss.buzzsprout.com/2260490.rss" podcast ruby)                                        ; Remote Ruby
+          ("https://bikeshed.thoughtbot.com/rss" podcast ruby)                                           ; The Bike Shed
+          ("https://www.therubyonrailspodcast.com/rss" podcast ruby rails)                               ; Ruby on Rails Podcast
 
           ;; Podcasts - Tech / Software
-          ("https://changelog.com/podcast/feed" podcast tech)               ; The Changelog
-          ("https://feed.syntax.fm/rss" podcast tech webdev)                ; Syntax
+          ("https://changelog.com/podcast/feed" podcast tech)                                            ; The Changelog
+          ("https://feed.syntax.fm/rss" podcast tech webdev)                                             ; Syntax
           ("https://feeds.transistor.fm/practical-ai-machine-learning-data-science-llm" podcast tech ai) ; Practical AI
 
           ;; Podcasts - Linux / Self-hosting
-          ("https://feeds.jupiterbroadcasting.com/lup" podcast linux)       ; LINUX Unplugged
-          ("https://feeds.fireside.fm/selfhosted/rss" podcast linux)        ; Self-Hosted
-          ("https://feeds.redcircle.com/f8fe71b6-911e-4abb-8fdc-cebda631a2da" podcast homeassistant) ; Home Assistant
+          ("https://feeds.jupiterbroadcasting.com/lup" podcast linux)                                    ; LINUX Unplugged
+          ("https://feeds.fireside.fm/selfhosted/rss" podcast linux)                                     ; Self-Hosted
+          ("https://feeds.redcircle.com/f8fe71b6-911e-4abb-8fdc-cebda631a2da" podcast homeassistant)     ; Home Assistant
 
           ;; Podcasts - Science / Interviews
-          ("https://feeds.megaphone.fm/hubermanlab" podcast science)        ; Huberman Lab
-          ("https://lexfridman.com/feed/podcast/" podcast interviews)       ; Lex Fridman
-          ("https://rss2.flightcast.com/xmsftuzjjykcmqwolaqn6mdn" podcast interviews) ; Diary of a CEO
+          ("https://feeds.megaphone.fm/hubermanlab" podcast science)                                     ; Huberman Lab
+          ("https://lexfridman.com/feed/podcast/" podcast interviews)                                    ; Lex Fridman
+          ("https://rss2.flightcast.com/xmsftuzjjykcmqwolaqn6mdn" podcast interviews)                    ; Diary of a CEO
 
           ;; Podcasts - Electronics
-          ("https://theamphour.com/feed/podcast/" podcast electronics)      ; The Amp Hour
+          ("https://theamphour.com/feed/podcast/" podcast electronics)                                   ; The Amp Hour
 
           ;; Podcasts - Other
-          ("https://evilgeniuschronicles.org/feed.xml" podcast)))           ; Evil Genius Chronicles
+          ("https://evilgeniuschronicles.org/feed.xml" podcast)))                                        ; Evil Genius Chronicles
 
   ;; Show entries from the last 2 weeks by default
   (setq elfeed-search-filter "@2-weeks-ago +unread")
